b9d9d9d032606945746c87145e936ec460789218
Resi Respati <resir014@gmail.com>
2016-02-20 17:32:49 +0700

    Initial commit

---
+++ bots.rb
@@ -0,0 +1,136 @@
+require 'twitter_ebooks'
+
+# Information about a particular Twitter user we know
+class UserInfo
+  attr_reader :username
+
+  # @return [Integer] how many times we can pester this user unprompted
+  attr_accessor :pesters_left
+
+  # @param username [String]
+  def initialize(username)
+    @username = username
+    @pesters_left = 1
+  end
+end
+
+class CloneBot < Ebooks::Bot
+  attr_accessor :original, :model, :model_path
+
+  def configure
+    # Configuration for all CloneBots
+    self.consumer_key = "sPKrpam6WiGPM7UELcc3RkYCf"
+    self.consumer_secret = "8gRcrTuxbqUt5z7Cg0fC49g0zguH4T7daWgRAyUCeJoWa3QWP2"
+    self.blacklist = []
+    self.delay_range = 1..6
+    @userinfo = {}
+  end
+
+  def top100; @top100 ||= model.keywords.take(100); end
+  def top20;  @top20  ||= model.keywords.take(20); end
+
+  def on_startup
+    load_model!
+
+    scheduler.cron '0 * * * *' do
+      # Every hour, post a single tweet
+      tweet(model.make_statement)
+    end
+  end
+
+  def on_message(dm)
+    delay do
+      reply(dm, model.make_response(dm.text))
+    end
+  end
+
+  def on_mention(tweet)
+    # Become more inclined to pester a user when they talk to us
+    userinfo(tweet.user.screen_name).pesters_left += 1
+
+    delay do
+      reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+    end
+  end
+
+  def on_timeline(tweet)
+    return if tweet.retweeted_status?
+    return unless can_pester?(tweet.user.screen_name)
+
+    tokens = Ebooks::NLP.tokenize(tweet.text)
+
+    interesting = tokens.find { |t| top100.include?(t.downcase) }
+    very_interesting = tokens.find_all { |t| top20.include?(t.downcase) }.length > 2
+
+    delay do
+      if very_interesting
+        favorite(tweet) if rand < 0.5
+        retweet(tweet) if rand < 0.1
+        if rand < 0.01
+          userinfo(tweet.user.screen_name).pesters_left -= 1
+          reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+        end
+      elsif interesting
+        favorite(tweet) if rand < 0.05
+        if rand < 0.001
+          userinfo(tweet.user.screen_name).pesters_left -= 1
+          reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+        end
+      end
+    end
+  end
+
+  # Find information we've collected about a user
+  # @param username [String]
+  # @return [Ebooks::UserInfo]
+  def userinfo(username)
+    @userinfo[username] ||= UserInfo.new(username)
+  end
+
+  # Check if we're allowed to send unprompted tweets to a user
+  # @param username [String]
+  # @return [Boolean]
+  def can_pester?(username)
+    userinfo(username).pesters_left > 0
+  end
+
+  # Only follow our original user or people who are following our original user
+  # @param user [Twitter::User]
+  def can_follow?(username)
+    @original.nil? || username == @original || twitter.friendship?(username, @original)
+  end
+
+  def favorite(tweet)
+    if can_follow?(tweet.user.screen_name)
+      super(tweet)
+    else
+      log "Unfollowing @#{tweet.user.screen_name}"
+      twitter.unfollow(tweet.user.screen_name)
+    end
+  end
+
+  def on_follow(user)
+    if can_follow?(user.screen_name)
+      follow(user.screen_name)
+    else
+      log "Not following @#{user.screen_name}"
+    end
+  end
+
+  private
+  def load_model!
+    return if @model
+
+    @model_path ||= "model/#{original}.model"
+
+    log "Loading model #{model_path}"
+    @model = Ebooks::Model.load(model_path)
+  end
+end
+
+CloneBot.new("resir014_ebooks") do |bot|
+  bot.access_token = "700914580835282945-Uzt6PjPXfmHu991GmZVpuVwlbIrdNIz"
+  bot.access_token_secret = "GHlg9a20UrRchLmesSIw7EGYop3PQdWmyApDWS2LqgtZ0"
+
+  bot.original = "resir014"
+end
